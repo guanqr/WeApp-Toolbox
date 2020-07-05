12f2979023d9ad57c34cc44a61c287944c162760
guanqirui <guanqirui@zju.edu.cn>
2020-07-05 12:32:39 +0800

    add person page

--- miniprogram/datas/toolList.js
+++ miniprogram/datas/toolList.js
@@ -1,27 +1,27 @@
 var json = [
   {
     "id": 1,
-    "name": "亲戚计算器",
-    "logo": "/images/calculat.png",
-    "url": "/pages/relationship-calculator/relationship-calculator"
+    "name": "温度统计单",
+    "logo": "/images/temperature.png",
+    "url": "/pages/temperature-list/temperature-list"
   },
   {
     "id": 2,
-    "name": "BMI 计算器",
-    "logo": "/images/weight.png",
-    "url": "/pages/bmi-calculator/bmi-calculator"
-  },
-  {
-    "id": 3,
     "name": "家庭记账本",
     "logo": "/images/relationship.png",
     "url": "/pages/cashbook/cashbook"
   },
   {
+    "id": 3,
+    "name": "BMI 计算器",
+    "logo": "/images/weight.png",
+    "url": "/pages/bmi-calculator/bmi-calculator"
+  },
+  {
     "id": 4,
-    "name": "温度统计单",
-    "logo": "/images/temperature.png",
-    "url": "/pages/temperature-list/temperature-list"
+    "name": "亲戚计算器",
+    "logo": "/images/calculat.png",
+    "url": "/pages/relationship-calculator/relationship-calculator"
   }
 ]
 
--- miniprogram/pages/setting/setting.js
+++ miniprogram/pages/setting/setting.js
@@ -1,18 +1,53 @@
 // pages/setting/setting.js
+
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    if (app.globalData.userInfo) {
+      that.setUserInfo(app.globalData.userInfo);
+    } else if (that.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setUserInfo(res.userInfo);
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          that.setUserInfo(res.userInfo);
+        }
+      })
+    }
+  },
 
+  getUserInfo: function (e) {
+    this.setUserInfo(e.detail.userInfo);
+  },
+
+  setUserInfo: function (userInfo) {
+    if (userInfo != null) {
+      app.globalData.userInfo = userInfo
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+    }
   },
 
   /**
